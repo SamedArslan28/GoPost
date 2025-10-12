ff9052a2878deff6cd9eaba1268201d3a960bb33
Abdulsamed Arslan <samedarslan0723@gmail.com>
2025-10-12 16:24:52 +0300

    feat: Error handling and duplicated email.

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,12 +3,6 @@ package errors
 import "errors"
 
 var (
-	ErrNotFound      = errors.New("not found")
 	ErrEmailConflict = errors.New("email already taken")
-	ErrUnauthorized  = errors.New("unauthorized")
-	ErrForbidden     = errors.New("forbidden")
-	ErrBadRequest    = errors.New("bad request")
-	ErrInternal      = errors.New("internal error")
-	ErrDuplicated    = errors.New("duplicated")
 	ErrParseBody     = errors.New("failed to parse body")
 )
